6d7fe3204c2d1b0040930514b4e287fa21cfcf0b
Feltrin Emanuele <Emanuelefeltrin19971010@gmail.com>
2019-01-19 09:48:01 +0100

    Programma funzionante, senza metodi

--- FeltrinEmanuele_a_Semaforo.ino
+++ FeltrinEmanuele_a_Semaforo.ino
@@ -1,13 +1,50 @@
     void setup() {
   // put your setup code here, to run once:
-  pinMode (10,OUTPUT);
+  
+  int rosso1 = 10;
+  int giallo1 = 9;
+  int verde1 = 8;
+  
+  pinMode (rosso1,OUTPUT);
+  pinMode (giallo1,OUTPUT);
+  pinMode (verde1,OUTPUT);
 
 
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (10,HIGH);
-  delay (1000);
-  digitalWrite (10,LOW);
-  delay(1000);
+  
+  int rosso1 = 10;
+  int giallo1 = 9;
+  int verde1 = 8;
+  
+  digitalWrite (rosso1,HIGH);
+  delay (2000);
+   digitalWrite (rosso1,LOW);
+  delay(0);
+  
+  digitalWrite (giallo1 ,HIGH);
+  delay (0);
+  digitalWrite (rosso1 ,HIGH);
+  delay (2000);
+  
+  digitalWrite (giallo1 ,LOW);
+  delay(0);
+  digitalWrite (rosso1 , LOW);
+  delay(0);
+  digitalWrite (verde1 , HIGH);
+  delay(4000);
+
+for (int i=0; i<5; i++)
+{
+  digitalWrite (verde1 , HIGH);
+  delay(300);
+  digitalWrite (verde1, LOW);
+  delay (400);
+}
+
+digitalWrite (giallo1, HIGH);
+delay(2000);
+digitalWrite (giallo1,LOW);
+delay(0);
 }
