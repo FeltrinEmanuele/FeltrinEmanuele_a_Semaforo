1ec502bede9972464c0b40728ee5eb9a68973487
FeltrinEmanuele <emanuelefeltrin19971010@gmail.com>
2019-02-01 23:39:17 +0100

    buon punto

--- FeltrinEmanuele_a_Semaforo.ino
+++ FeltrinEmanuele_a_Semaforo.ino
@@ -7,6 +7,8 @@
   int rosso2 = 11;
   int giallo2 = 12;
   int verde2 = 13;
+  int temporosso;
+  int lampeggiverde;
   
   
   pinMode (rosso1,OUTPUT);
@@ -30,10 +32,14 @@ void loop() {
   int rosso2 = 11;
   int giallo2 = 12;
   int verde2 = 13;
+  int temporosso;
+  int lampeggiverde;
+  int tempolampeggiverde;
+  
   
   //fase 1 [IL ROSSO STA ACCESO PER X TEMPO]
   digitalWrite (rosso1,HIGH);
-  delay (2000); //variabile
+  delay (temporosso); //variabile
   digitalWrite (rosso1,LOW);
   delay(0);
 
@@ -41,26 +47,26 @@ void loop() {
   digitalWrite (giallo1 ,HIGH);
   delay (0);
   digitalWrite (rosso1 ,HIGH);
-  delay (2000);//variabile
+  delay (2000); //variabile
   digitalWrite (giallo1 ,LOW);
   delay(0);
   digitalWrite (rosso1 , LOW);
-  delay(0);
+  delay(0); 
 
-  //fase 3
+  //fase 3 [IL VERDE SI ACCENDE E RESTA ACCESO CON UN DELAY CHE SI CALCOLA CON: ROSSO(della fase 1) - Lampeggio(della fase 4) * durata lampeggio(della fase 4)]
   digitalWrite (verde1 , HIGH);
-  delay(4000);
+  delay(temporosso-lampeggiverde*tempolampeggiverde*2); //variabile
 
-//fase 4
-for (int i=0; i<5; i++)
+//fase 4[LAMPEGGIA X VOLTE E PER X SECONDI]
+for (int i=0; i<lampeggiverde; i++)
 {
   digitalWrite (verde1 , HIGH);
-  delay(300);
+  delay(tempolampeggiverde); //variabile
   digitalWrite (verde1, LOW);
-  delay (400);
+  delay (tempolampeggiverde); //variabile
 }
 
-//fase 5
+//fase 5[IL GIALLO RESTA ACCESO PER X SECONDI]
 digitalWrite (giallo1, HIGH);
 delay(2000);
 digitalWrite (giallo1,LOW);
