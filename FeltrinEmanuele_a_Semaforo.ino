a0fafa2e53de21ab8087246b496cc2b81a5f9214
Feltrin Emanuele <Emanuelefeltrin19971010@gmail.com>
2019-02-02 09:36:10 +0100

    finito

--- FeltrinEmanuele_a_Semaforo.ino
+++ FeltrinEmanuele_a_Semaforo.ino
@@ -14,12 +14,12 @@
   void setup() {
     
   // put your setup code here, to run once:
-  int rosso1 = 10;
-  int giallo1 = 9;
-  int verde1 = 8;
-  int rosso2 = 11;
-  int giallo2 = 12;
-  int verde2 = 13;
+  rosso1 = 10;
+  giallo1 = 9;
+  verde1 = 8;
+  rosso2 = 11;
+  giallo2 = 12;
+  verde2 = 13;
  
   pinMode (rosso1,OUTPUT);
   pinMode (giallo1,OUTPUT);
@@ -29,6 +29,11 @@
   pinMode (verde2,OUTPUT);
 
   Serial.begin(9600);
+
+  ILtempolampeggiverde();
+  ILlampeggiverde();
+  ILtempogiallo();
+  ILtemposemaforo();
   
   }
   
@@ -36,20 +41,19 @@ void loop()
 {
   // put your main code here, to run repeatedly:
 
+ 
+
   if (temposemaforo < tempogiallo)
   {"la variabile tempogiallo è troppo grande";}
 
-  temporosso = temposemaforo - tempogiallo;
-  tempoverde = temporosso-lampeggiverde*tempolampeggiverde*2;
+  int temporosso = temposemaforo - tempogiallo;
+  int tempoverde = temporosso-lampeggiverde*tempolampeggiverde*2; 
   
- 
+ Serial.print("INIZIO");
   //fase 1 [IL ROSSO STA ACCESO PER X TEMPO]
   digitalWrite (rosso1,HIGH);
-  delay (0);
   digitalWrite (verde2,HIGH);
   delay(tempoverde);
-  digitalWrite (verde2, LOW);
-  delay(0);
   for (int i=0; i<lampeggiverde; i++)
   {
   digitalWrite (verde2 , HIGH);
@@ -57,29 +61,18 @@ void loop()
   digitalWrite (verde2, LOW);
   delay (tempolampeggiverde); //variabile
   }
-  delay (0);
-  digitalWrite (rosso1,LOW);
-  delay(0);
-  
 
   //fase 2 [IL ROSSO E GIALLO STANNO ACCESI PER X TEMPO]
   digitalWrite (giallo1 ,HIGH);
-  delay (0);
   digitalWrite (giallo2 ,HIGH);
-  delay (0);
-  digitalWrite (rosso1 ,HIGH);
   delay (tempogiallo); //variabile
   digitalWrite (giallo1 ,LOW);
-  delay(0);
   digitalWrite (giallo2 ,LOW);
-  delay (0);
   digitalWrite (rosso1 , LOW);
-  delay(0); 
 
- 
+
   //fase 3 [IL VERDE SI ACCENDE E RESTA ACCESO CON UN DELAY CHE SI CALCOLA CON: ROSSO(della fase 1) - Lampeggio(della fase 4) * durata lampeggio(della fase 4)]
   digitalWrite (rosso2, HIGH);
-  delay (0);
   digitalWrite (verde1 , HIGH);
   delay(tempoverde);
   digitalWrite (verde1, LOW);
@@ -114,7 +107,7 @@ void loop()
 
 void ILtempolampeggiverde()
 {
-  Serial.print("con quale velocità vuoi i lampeggi verdi (ms)?");
+  Serial.println("con quale velocità vuoi i lampeggi verdi (ms)?");
   while(Serial.available() == 0) {};
   String inp = Serial.readString(); 
   tempolampeggiverde = inp.toInt();
@@ -122,7 +115,7 @@ void ILtempolampeggiverde()
 
 void ILlampeggiverde()
 {
-  Serial.print("quanti lampeggi verdi?");
+  Serial.println("quanti lampeggi verdi?");
   while(Serial.available() == 0) {};
   String inp =Serial.readString();
   lampeggiverde = inp.toInt();
@@ -130,7 +123,7 @@ void ILlampeggiverde()
 
 void ILtempogiallo()
 {
-  Serial.print("per quanto tempo deve stare giallo il semaforo?");
+  Serial.println("per quanto tempo deve stare giallo il semaforo?");
   while(Serial.available() == 0) {};
   String inp =Serial.readString();
   tempogiallo =  inp.toInt();
@@ -138,7 +131,7 @@ void ILtempogiallo()
 
 void ILtemposemaforo()
 {
-  Serial.print("con quale tempo vuoi che abbia finito un ciclo? ");
+  Serial.println("con quale tempo vuoi che abbia finito un ciclo? ");
   while  (Serial.available() == 0) {};
   String inp = Serial.readString();
   temposemaforo = inp.toInt();
